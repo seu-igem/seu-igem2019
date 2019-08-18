23588cf5fa81894b1a19ad6522fad0631af8e945
X.Y.Z <g.for.xyz@gmail.com>
2019-08-18 14:56:06 +0800

    1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense, lazy } from 'react';
+import { Route, Switch } from 'react-router';
+
 import './App.css';
+import Loading from './comps/Loading';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div id='App'>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            <Route exact
+              path='/'
+              component={lazy(() => import('./pages/Home'))}
+            />
+            <Route exact
+              path='/Description'
+              component={lazy(() => import('./pages/Description'))}
+            />
+          </Switch>
+
+        </Suspense>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/comps/Loading.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Loading.css';
+
+export default class Loading extends React.Component {
+  public render() {
+    return (
+      <div className='loading-container'>
+        <div className='sk-cube sk-cube1'></div>
+        <div className='sk-cube sk-cube2'></div>
+        <div className='sk-cube sk-cube3'></div>
+        <div className='sk-cube sk-cube4'></div>
+        <div className='sk-cube sk-cube5'></div>
+        <div className='sk-cube sk-cube6'></div>
+        <div className='sk-cube sk-cube7'></div>
+        <div className='sk-cube sk-cube8'></div>
+        <div className='sk-cube sk-cube9'></div>
+      </div>
+    );
+  }
+}
