874ad45c4706430c75811add94563887edaa5273
X.Y.Z <g.for.xyz@gmail.com>
2019-08-20 15:01:50 +0800

    fix(wiki-tools/login): should wait until page closed

--- wiki-tools/login.ts
+++ wiki-tools/login.ts
@@ -4,14 +4,14 @@ const { account } = config;
 import { delay } from './util';
 
 export default async (browser: pupp.Browser) => {
-  const page = await browser.newPage();
-  await page.goto('https://igem.org/Login2');
-  await page.type('input[type=text]', account.username);
-  await page.type('input[type=password]', account.password);
+   const page = await browser.newPage();
+   await page.goto('https://igem.org/Login2');
+   await page.type('input[type=text]', account.username);
+   await page.type('input[type=password]', account.password);
 
-  await Promise.all([
-    page.click('.submit'),
-    page.waitForNavigation({ waitUntil: 'networkidle0' }),
-  ]);
-  page.close();
+   await Promise.all([
+      page.click('.submit'),
+      page.waitForNavigation({ waitUntil: 'networkidle0' }),
+   ]);
+   await page.close();
 };
