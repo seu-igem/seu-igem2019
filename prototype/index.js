6150977fc80f3bf765f74ab33c1ee639225d42f1
X.Y.Z <g.for.xyz@gmail.com>
2019-08-22 20:41:28 +0800

    chore(prototype): conceived a web page prototype

---
+++ prototype/index.js
@@ -0,0 +1,6 @@
+const d = document;
+const w = window;
+
+// const rootDiv = d.createElement('div');
+// rootDiv.setAttribute('id', 'root');
+// d.getElementsByTagName('body')[0].appendChild(rootDiv);
