60eae1e6bb1eb1e9d3004b8578c2c994f9c7e61d
X.Y.Z <g.for.xyz@gmail.com>
2019-09-04 03:16:15 +0800

    fix(wiki-tools/upload): file path on iGEM shouldn't contain dot

--- wiki-tools/upload.ts
+++ wiki-tools/upload.ts
@@ -8,58 +8,59 @@ import { delay, concurrencyRestrictify } from './util';
 
 const selector = '../dist/**/*.{js,css}';
 interface File2BeUploaded {
-  path: string;
-  content: string;
+   path: string;
+   content: string;
 }
 
 (async () => {
-  const browser = await pupp.launch({ headless: false });
-  await login(browser);
+   const browser = await pupp.launch({ headless: false });
+   await login(browser);
 
-  const files = searchTargetFiles();
-  let succeedNum = 0;
+   const files = searchTargetFiles();
+   let succeedNum = 0;
 
-  const singleUpload = concurrencyRestrictify(_singleUpload, 2);
+   const singleUpload = concurrencyRestrictify(_singleUpload, 2);
 
-  await Promise.all(
-    files.map(file => singleUpload(file))
-  );
+   await Promise.all(
+      files.map(file => singleUpload(file))
+   );
 
-  async function _singleUpload(file: File2BeUploaded) {
-    const page = await browser.newPage();
-    console.log(file.path + ': 前往编辑页面...');
-    await page.goto(`${config.igemUrl}/wiki/index.php?title=Team:${config.teamName}/${file.path}&action=edit`, {
-      waitUntil: 'domcontentloaded',
-    });
-    console.log(file.path + ': 抵达编辑页面...');
-    await page.$eval(
-      '.mw-ui-input',
-      (el: any, content: string) => { el.value = content },
-      file.content
-    );
+   async function _singleUpload(file: File2BeUploaded) {
+      const page = await browser.newPage();
+      console.log(file.path + ': 前往编辑页面...');
+      const pathAtIGEM = file.path.replace(/\./g, '-');
+      await page.goto(`${config.igemUrl}/wiki/index.php?title=Team:${config.teamName}/${pathAtIGEM}&action=edit`, {
+         waitUntil: 'domcontentloaded',
+      });
+      console.log(file.path + ': 抵达编辑页面...');
+      await page.$eval(
+         '.mw-ui-input',
+         (el: any, content: string) => { el.value = content },
+         file.content
+      );
 
-    console.log(file.path + ': 提交...');
-    await Promise.all([
-      page.click('#wpSave'),
-      page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
-    ]);
-    console.log(file.path + ': 提交成功 (' + (++succeedNum) + '/' + files.length + ')');
-    await page.close();
-  }
+      console.log(file.path + ': 提交...');
+      await Promise.all([
+         page.click('#wpSave'),
+         page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
+      ]);
+      console.log(file.path + ': 提交成功 (' + (++succeedNum) + '/' + files.length + ')');
+      await page.close();
+   }
 
-  await browser.close();
+   await browser.close();
 })().catch(r => console.error(r));
 
 function searchTargetFiles() {
-  const targetFiles: File2BeUploaded[] = [];
+   const targetFiles: File2BeUploaded[] = [];
 
-  glob.sync(selector)
-    .forEach(path => {
-      targetFiles.push({
-        path: path.substring(8),
-        content: fs.readFileSync(path, 'utf8')
+   glob.sync(selector)
+      .forEach(path => {
+         targetFiles.push({
+            path: path.substring(8),
+            content: fs.readFileSync(path, 'utf8')
+         });
       });
-    });
 
-  return targetFiles;
+   return targetFiles;
 }
